ea2c8dbb3c7abebf172a2ccab68211213641871d
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-14 13:38:36 +0400

    add Asp Api :ok:

---
+++ SimpleAPP/SimpleAPP/Controlers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleAPP.Controlers
+{
+    [RoutePrefix("api")]
+    public class ValuesController : ApiController
+    {
+
+        [Route("values")]
+        [HttpGet]
+        public IHttpActionResult GetValues()
+        {
+            var resulit = new string[]
+            {
+                "Ախ ոտս",
+                "Տեղ տուր նստեմ"
+            };
+            return Ok();
+        }
+    }
+}
---
+++ SimpleAPP/SimpleAPP/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Microsoft.Owin;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(SimpleAPP.Startup))]
+
+namespace SimpleAPP
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage("/");
+
+            HttpConfiguration httpConfig = new HttpConfiguration();
+
+            ConfigureWebApi(httpConfig);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(httpConfig);
+        }
+
+        private void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
+
