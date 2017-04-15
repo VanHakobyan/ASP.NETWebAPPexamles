83e5bfc09fa7540215d6249c7fdd7147810148c7
VanHakobyan <van19962013@mail.ru>
2017-04-15 12:03:47 +0400

    add Global.asax :two:

---
+++ SmallProjects/Routing/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Routing
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute("MyRoute", "{controller}");
+        }
+    }
+}
