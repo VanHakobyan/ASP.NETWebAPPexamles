6e541a424a90c8ec137affd97143707cbdb26bcb
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-16 01:29:14 +0400

    add new MVC web api controllers :9::1:

---
+++ SmallProjects/Routing2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Routing2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SmallProjects/Routing2/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Routing2.Controllers
+{
+    public class FirstController : ApiController
+    {
+        [HttpGet]
+        public string SayHi()
+        {
+            return "hi";
+        }
+    }
+}
