9b83a76261800c374abe53fac3ebe0c3b60f4a65
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-16 12:37:40 +0400

    add 404 :smile:

---
+++ SmallProjects/WebApiSample/Controllers/FruitController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiSample.Controllers
+{
+    public class FruitController : ApiController
+    {
+        static string[] fruit =
+        {
+            "Տանձ",
+            "խնձոր",
+            "Մանդարին",
+            "Մնացածը պարզա !!!"
+        };
+        public IEnumerable<string> get()
+        {
+            return fruit;
+        }
+        //public string Get(int id)
+        //{
+        //    return fruit[id];
+        //}
+        public HttpResponseMessage Get(int id)
+        {
+            if (id > fruit.Length)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, fruit[id]);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Լուրջ մտածում էիր որ պետքա լինի?");
+            }
+        }
+    }
+}
