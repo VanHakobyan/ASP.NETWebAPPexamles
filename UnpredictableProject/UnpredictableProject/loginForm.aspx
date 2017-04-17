9f6ea19acb2cd9bdabe2243413959a4df44b052e
VanHakobyan <van19962013@mail.ru>
2017-04-17 22:30:07 +0400

    add post method realizatino

---
+++ UnpredictableProject/UnpredictableProject/loginForm.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="loginForm.aspx.cs" Inherits="UnpredictableProject.loginForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Login <asp:Label ID="LableLogin" runat="server"></asp:Label><br/>
+            Password <asp:Label ID="LablePassword" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UnpredictableProject/UnpredictableProject/loginForm.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UnpredictableProject
+{
+    public partial class loginForm : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string login, password;
+            login = Request.Form["loginParam"];
+            password = Request.Form["passwordParam"];
+
+            if (string.IsNullOrEmpty(login))
+            {
+                LableLogin.Text = "not parametr login";
+            }
+            else
+            {
+                LableLogin.Text = login;
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                LablePassword.Text = "not parametr password";
+            }
+            else
+            {
+                LablePassword.Text = login;
+            }
+        }
+    }
+}
